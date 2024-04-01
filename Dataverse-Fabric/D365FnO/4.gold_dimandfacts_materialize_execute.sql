7c77e626642303cffdb45105a5c452b27bc27c22
JJ Yadav <65608469+jiyada@users.noreply.github.com>
2024-04-01 16:29:23 -0500

    Add files via upload

---
+++ Dataverse-Fabric/D365FnO/4.gold_dimandfacts_materialize_execute.sql
@@ -0,0 +1,5 @@
+
+execute Legacy_AX_2012_Data.[dbo].[load_customers]
+execute Legacy_AX_2012_Data.[dbo].[load_legalentity]
+execute Legacy_AX_2012_Data.[dbo].[load_products]
+execute Legacy_AX_2012_Data.[dbo].[load_salesorderdetails]
