4e600cb383f2c4e3c5495743d9cc2c0d91f8cecf
Scott Sewell <mscottsewell@users.noreply.github.com>
2024-04-12 07:03:30 -0500

    Moved FnO folder to root level

--- Dataverse-Fabric/D365FnO/4.gold_dimandfacts_materialize_execute.sql
+++
@@ -1,5 +0,0 @@
-
-execute Legacy_AX_2012_Data.[dbo].[load_customers]
-execute Legacy_AX_2012_Data.[dbo].[load_legalentity]
-execute Legacy_AX_2012_Data.[dbo].[load_products]
-execute Legacy_AX_2012_Data.[dbo].[load_salesorderdetails]
